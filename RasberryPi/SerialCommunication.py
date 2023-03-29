89908bb27513021448810e29e79d5512a15a74ee
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-29 23:49:14 +0200

    Serial

--- RasberryPi/SerialCommunication.py
+++ RasberryPi/SerialCommunication.py
@@ -2,7 +2,17 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
+port = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
 time.sleep(3)
-ser.reset_input_buffer()
+port.reset_input_buffer()
 print("Serial OK")
+
+try:
+	while True:
+		time.sleep(0.01)
+		if port.in_waiting > 0:
+			line = port.readline().decode('utf-8').rstrip()
+			print(line)
+except KeyboardInterrupt:
+	print("Closing Serial Communication")
+	port.close()
