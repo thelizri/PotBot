41d7c0b933b0d9232129d6bc088f23855b40b5c9
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2023-04-20 09:25:40 +0200

    add simple skeleton for presenter components

--- pot-bot-app/src/presenters/loginPresenter.js
+++ pot-bot-app/src/presenters/loginPresenter.js
@@ -1,1 +1,12 @@
 /* UseState over here */
+
+import React from 'react';
+import LoginView from "../views/loginView";
+
+function LoginPresenter() {
+    return (
+        <LoginView/>
+    );
+}
+
+export default LoginPresenter;
--- pot-bot-app/src/presenters/signUpPresenter.js
+++ pot-bot-app/src/presenters/signUpPresenter.js
@@ -1,1 +1,11 @@
 /* UseState over here */
+import React from 'react';
+import SignupView from "../views/signUpView";
+
+function SignUpPresenter() {
+    return (
+        <SignupView/>
+    );
+}
+
+export default SignUpPresenter;
