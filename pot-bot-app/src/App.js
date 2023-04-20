9e178567fca20f86b26ef2551bdc3ac10f941dd7
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2023-04-20 09:25:40 +0200

    add simple skeleton for presenter components

--- pot-bot-app/src/App.js
+++ pot-bot-app/src/App.js
@@ -1,28 +1,20 @@
-import LoginView from "./views/loginView";
-import SignUpView from "./views/signUpView";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import LoginPresenter from "./presenters/loginPresenter";
+import SignUpPresenter from "./presenters/signUpPresenter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/" element={<LoginView />} />
-            <Route path="/signup" element={<SignUpView />} />
-          </Routes>
-        </Router>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<LoginPresenter/>}/>
+                        <Route path="/signup" element={<SignUpPresenter/>}/>
+                    </Routes>
+                </Router>
+            </header>
+        </div>
+    );
 }
 
 export default App;
