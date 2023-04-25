41fe396735d5c9293c4ff8e62fa8e2109aab067f
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2023-04-25 13:50:49 +0200

    move logic from view to presenter

--- pot-bot-app/src/presenters/PlantPresenter.js
+++ pot-bot-app/src/presenters/PlantPresenter.js
@@ -1,28 +1,28 @@
-import {Link } from "react-router-dom";
 import {useAuth, readUserData, hasPlants} from "../firebaseModel";
 import React, {useEffect, useState} from "react";
 import PlantView from "../views/PlantView";
 import AddPlantView from "../views/AddPlantView";
 
 export default function PlantPresenter() {
-    const [plantss, setPlants] = useState([])
+    const [plants, setPlants] = useState([]);
+
     const {user} = useAuth();
-    const getPlants = readUserData(user,"plants");
-    //console.log(getPlants)
     let hasPlantPromise = hasPlants(user);
     const [hasPlant, setPlantBool] = useState(false);
     hasPlantPromise.then((v) => {
         console.log(v + " hasPlantPromise");
         setPlantBool(v);
     }).catch(err => console.error(err));
-    console.log(hasPlant)
 
-    return (
+    useEffect(() => {
+        if (plants.length === 0) {
+            readUserData(user, "plants").then((data => {
+                setPlants(data)
+            }));
+        }
+    }, [user])
 
-            <div>
-                {hasPlant? <PlantView plants={getPlants} /> : <AddPlantView/>}
-            </div>
-
-    )
-
+    return <div>
+        {hasPlant? <PlantView plants={plants}/> : <AddPlantView/>}
+    </div>
 }
--- pot-bot-app/src/views/PlantView.js
+++ pot-bot-app/src/views/PlantView.js
@@ -1,39 +1,12 @@
-import {useAuth} from "../firebaseModel";
-import React, {useEffect, useState} from "react";
+import React from "react";
 
-
-export default function PlantView(props) {
-    const {user} = useAuth();
-    let plantPromise = props.plants;
-    const [plants, setPlants] = useState([]);
-    console.log(plants)
-
-
-    useEffect(() => {
-        if (plants.length == 0) {
-            plantPromise.then((data => {
-                setPlants(data)
-                console.log(data)}
-        ));
-        }
-    }, [user, plants])
-
-    function renderPlants(plant) {
-        console.log(plant)
-        return (
-            <div>
-                <h2>{`${plant.key}`}</h2>
-            </div>
-        )
-    }
-    return (
-        <div>
-            <h1>Plants</h1>
-            <p style={{align: 'center'}}>get plants from db in list</p>
-            {plants && Object.keys(plants).map(x => <p>{x}</p>)}
-
-        </div>
-    )
-
-
+export default function PlantView({plants}) {
+  return (
+    <div>
+      <h1>Your plants</h1>
+      <ul>
+        {plants && Object.keys(plants).map(x => <li key={x}>{x}</li>)}
+      </ul>
+    </div>
+  )
 }
