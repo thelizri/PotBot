1425832945775126280acc92733de8f30afd0c46
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-26 14:02:43 +0200

    Update

--- RasberryPi/cloud_database_communicator.py
+++ RasberryPi/cloud_database_communicator.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 # Replace 'path/to/your-service-account-key.json' with the path to the JSON file you downloaded
-cred = credentials.Certificate('firebase-key.json')
+cred = credentials.Certificate('C:\\Users\\karlw\\Documents\\Code\\PotBot\\RasberryPi\\firebase-key.json') #Replace with real path for the raspberry pi
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/'
