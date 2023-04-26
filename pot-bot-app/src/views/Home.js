03a95855cda387bf4eee7a4bcf7b7f6cedbc20e9
vrncndlr <veronica.j.nadler@gmail.com>
2023-04-26 14:51:28 +0200

    plant-view/home page edited

--- pot-bot-app/src/views/Home.js
+++ pot-bot-app/src/views/Home.js
@@ -1,12 +1,18 @@
 import {Link } from "react-router-dom";
 import {useAuth} from "../firebaseModel";
+import { useState } from "react";
 import React from "react";
+import '../styling/homeView.css';
+
 export default function Home() {
     const {user, logOut} = useAuth();
+    
     //console.log(user);
     return (
         <div className="Home">
-            {user && <Welcome/> }
+            {user && <Welcome/>}
+            {user && <Plants/>}
+            {user && <LogoutBtn/>}
             {!user && <Login/>}
         </div>
     );
@@ -14,14 +20,34 @@ export default function Home() {
         return (
             <>
                 <h1>To access this page you have to login</h1> <Link to="/"><h2>Back to login</h2></Link>
-
             </>)
     }
     function Welcome(){
         return (
             <>
-                <h1>Welcome {user.email}</h1><Link to="/"><h2><button onClick={logOut}>Logout</button></h2></Link>
+            <div className="welcome">
+                <h2>Welcome {user.email}</h2>
+            </div>
             </>
+        )
+    }
+    function LogoutBtn(){
+        return(
+            <div className="logout">
+                <button to="/" onClick={logOut}>Log out</button>
+            </div>
+        )
+    }
+    function Plants(){
+        const [expanded, setExpanded] = useState(false);
+
+        function handleClick() {
+            setExpanded(!expanded);
+        }
+        return(
+            <div className={`expandable-div ${expanded ? "expanded" : ""}`} onClick={handleClick}>
+                <img src="pot-bot-app/src/styling/images/elefantora.jpg" alt="plant1" width="100" height="100"></img>
+            </div>
         )
     }
 }
