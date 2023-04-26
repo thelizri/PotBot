507f03b5d318848f450b506cd678583e778437b5
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-26 14:46:33 +0200

    Update

--- RasberryPi/cloud_database_communicator.py
+++ RasberryPi/cloud_database_communicator.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import json
 
 # Replace 'path/to/your-service-account-key.json' with the path to the JSON file you downloaded
 cred = credentials.Certificate('C:\\Users\\karlw\\Documents\\Code\\PotBot\\RasberryPi\\firebase-key.json') #Replace with real path for the raspberry pi
@@ -11,16 +12,16 @@ firebase_admin.initialize_app(cred, {
 
 def push_data(data):
     # Replace 'your_database_path' with the path where you want to push the data
-    ref = db.reference('RaspberryPiTest')
+    ref = db.reference('RaspberryPiSensorData')
     ref.push(data)
 
-# Sample data to be pushed
-sample_data = {
-    'name': 'Otto Doe',
-    'email': 'ottodoe@example.com',
-    'age': 90
-}
+def read_json_and_push(filepath, product_id):
+    file = open(filepath)
+    data = json.load(file)
+    file.close()
 
-push_data(sample_data)
+    for measurement in data['measurements']:
+        measurement['product-id'] = product_id
+        push_data(measurement)
 
-
+read_json_and_push('example.json', '1-test-raspberry')
