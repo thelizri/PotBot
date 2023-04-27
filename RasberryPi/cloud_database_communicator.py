135708892053d9f2f24300f683916928f1b366c9
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-27 15:48:02 +0200

    update

--- RasberryPi/cloud_database_communicator.py
+++ RasberryPi/cloud_database_communicator.py
@@ -13,8 +13,7 @@ firebase_admin.initialize_app(cred, {
 def push_data(data, product_id):
     # Replace 'your_database_path' with the path where you want to push the data
     ref = db.reference('/users/ffJEWDC2nfMi6BFu7fS1mKkRXnC3/plants/Parasollpilea/measureData')
-    child = ref.child(data["dateAndTime"])
-    child.update(data)
+    ref.update(data)
 
 def read_json_and_push(filepath, product_id):
     file = open(filepath)
@@ -24,6 +23,9 @@ def read_json_and_push(filepath, product_id):
     #Deletes content
     with open(filepath, 'w'):
         pass
+    
+    if data is None:
+        return
 
     data['product-id'] = product_id
     push_data(data, product_id)
