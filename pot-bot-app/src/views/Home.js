c92f0f18a575f2ae79a82eba49a34d50583a8633
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2023-04-27 17:12:25 +0200

    Plants in separate component file

--- pot-bot-app/src/views/Home.js
+++ pot-bot-app/src/views/Home.js
@@ -1,90 +1,58 @@
 import {Link, useNavigate} from "react-router-dom";
 import {useAuth} from "../firebaseModel";
-import { useState, useEffect } from "react";
 import React from "react";
-import {get, getDatabase, onValue, ref} from "firebase/database";
 import "firebase/database";
 import '../styling/homeView.css';
-import elefant from '../styling/images/elefant.jpg'
+import Plants from "./Plants";
 
 export default function Home() {
-    const {user, logOut, updateProfileName} = useAuth();
-    const userName = user.displayName;
-    const navigate = useNavigate()
-    
-    return (
-        <div className="Home">
-            {user && <Welcome/>}
-            {user && <Plants title="Parasollpilea"/>}
-            {user && <LogoutBtn/>}
-            {!user && <Login/>}
-        </div>
-    );
-    function Login(){
-        return (
-            <>
-                <h1>To access this page you have to login</h1> <Link to="/"><h2>Back to login</h2></Link>
-            </>)
-    }
-    function Welcome(){
-        function handlePlantClick(){
-            navigate("/home/plants")
-        }
-        async function changeUserName(){
-            let win = await window.open("/name", "_blank", "toolbar=no, resizable=no, height:100, width:200");
-        }
-        return (
-            <>
-                <h2>Welcome {userName}</h2>
-                {/*this button could be changed to just circle trough all plants instead*/}
-                {/* <button onClick={handlePlantClick}>Plants</button> */}
-                <button className="change-username" onClick={changeUserName}>Change User name</button>
-                {/* <Link to="/"><h2><button onClick={logOut}>Logout</button></h2></Link> */}
-            </>
-        )
-    }
-    
-    function LogoutBtn(){
-        return(
-            <div className="logout">
-                <button className="logout-btn" to="/" onClick={logOut}>Log out</button>
-            </div>
-        )
-    }
-    function Plants(props){
-        const [expanded, setExpanded] = useState(false);
-        const [soilMoisture, setSoilMoisture] = useState(0);
-        const [plant, setPlant] = useState({});
-        useEffect(() => {
-            const db = getDatabase();
-            const plantRef = ref(db, 'SensorData/raspberry-1/-NTxnhQiZqz3-ETWTALT');
-            get(plantRef).then((data) => setPlant(data.val()) )
-            // onValue(soilMoistureRef, (snapshot) => {
-            //     const data = snapshot.val();
-            //     setSoilMoisture(data);
-            //     console.log(data)
-            // })
-        }, []);
+  const {user, logOut} = useAuth();
+  const userName = user.displayName;
+  const navigate = useNavigate()
 
-        function handleClick() {
-            setExpanded(!expanded);
-        }
-        return(
-            <div>
-                <div className={`expandable-div ${expanded ? "expanded" : ""}`} onClick={handleClick}>
-                    <div className="card-title">
-                        <img src={elefant} width="100" height="100"/>
-                        <p>{props.title}</p>
-                    </div>
-                    {expanded && <div className="plant-data soil-moisture temperature uv-light">
-                                    <div>Moisture: {plant.soil_moisture} </div>
-                                    <div>Light: {plant.uv_light}</div>
-                                    <div>Temperature: {plant.temperature}</div>
-                                </div>}
-                </div>
-            </div>
-            
-        )
+  return (
+    <div className="Home">
+      {user && <Welcome/>}
+      {user && <Plants title="Parasollpilea"/>}
+      {user && <LogoutBtn/>}
+      {!user && <Login/>}
+    </div>
+  );
+
+  function Login() {
+    return (
+      <>
+        <h1>To access this page you have to login</h1> <Link to="/"><h2>Back to login</h2></Link>
+      </>)
+  }
+
+  function Welcome() {
+    function handlePlantClick() {
+      navigate("/home/plants")
     }
-                
+
+    async function changeUserName() {
+      let win = await window.open("/name", "_blank", "toolbar=no, resizable=no, height:100, width:200");
+    }
+
+    return (
+      <>
+        <h2>Welcome {userName}</h2>
+        {/*this button could be changed to just circle trough all plants instead*/}
+        {/* <button onClick={handlePlantClick}>Plants</button> */}
+        <button className="change-username" onClick={changeUserName}>Change User name</button>
+        {/* <Link to="/"><h2><button onClick={logOut}>Logout</button></h2></Link> */}
+      </>
+    )
+  }
+
+  function LogoutBtn() {
+    return (
+      <div className="logout">
+        <button className="logout-btn" to="/" onClick={logOut}>Log out</button>
+      </div>
+    )
+  }
+
+
 }
---
+++ pot-bot-app/src/views/Plants.jsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from "react";
+import {get, getDatabase, ref} from "firebase/database";
+import elephant from '../styling/images/elefant.jpg'
+import addPlantIcon from '../styling/images/plus-pot.png'
+import {useAuth} from '../firebaseModel'
+
+function Plants({title}) {
+
+  const [expanded, setExpanded] = useState(false);
+  const [plantObjects, setPlantObjects] = useState([]);
+  const {user} = useAuth();
+
+  useEffect(() => {
+    const db = getDatabase();
+    const plantRef = ref(db, `users/${user.uid}/plants`);
+    get(plantRef).then((data) => setPlantObjects(Object.values(data.val())))
+  }, []);
+
+  function handleClick() {
+    setExpanded(!expanded);
+  }
+
+  return (
+    <>
+      <div className={`expandable-div ${expanded ? "expanded" : ""}`}
+           onClick={handleClick}>
+        <div className="card-title">
+          <img src={elephant} width="100" height="100"/>
+          <span style={{fontFamily: "sans-serif", padding: "0.5em"}}>{title}</span>
+        </div>
+        {expanded && <div className="plant-data soil-moisture temperature uv-light">
+          <div>Moisture: dummy</div>
+          <div>Light: dummy</div>
+          <div>Temperature: dummy</div>
+        </div>}
+      </div>
+      <div className={"addPlant"}>
+        <img src={addPlantIcon}/>
+      </div>
+    </>)
+}
+
+export default Plants;
