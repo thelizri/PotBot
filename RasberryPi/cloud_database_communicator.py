835e202fe7c007e777be7f9207c4f11976445c3f
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-28 16:08:28 +0200

    Black Formatter

--- RasberryPi/cloud_database_communicator.py
+++ RasberryPi/cloud_database_communicator.py
@@ -13,26 +13,29 @@ os.chdir(abspath)
 
 # Replace 'path/to/your-service-account-key.json' with the path to the
 # JSON file you downloaded
-cred = credentials.Certificate('/home/pi/PotBot/RasberryPi/firebase-key.json')
+cred = credentials.Certificate("/home/pi/PotBot/RasberryPi/firebase-key.json")
 # cred = credentials.Certificate('C:\\Users\\karlw\\Documents\\Code\\PotBot\\firebase-key.json')
 
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/'
-})
+firebase_admin.initialize_app(
+    cred,
+    {
+        "databaseURL": "https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/"
+    },
+)
 
 
 def push_data(data):
     # Replace 'your_database_path' with the path where you want to push the
     # data
     ref = db.reference(
-        '/users/ffJEWDC2nfMi6BFu7fS1mKkRXnC3/plants/Parasollpilea/measureData')
-    child = ref.child(data['date'])
-    grandchild = child.child(data['time'])
+        "/users/ffJEWDC2nfMi6BFu7fS1mKkRXnC3/plants/Parasollpilea/measureData"
+    )
+    child = ref.child(data["date"])
+    grandchild = child.child(data["time"])
     grandchild.update(data)
 
 
 def read_json_and_push(filepath, product_id):
-
     if True:
         print("Cloud Start")
         if not os.path.exists(filepath):
@@ -53,18 +56,18 @@ def read_json_and_push(filepath, product_id):
         # with open(filepath, 'w'):
         #    pass
 
-        data['product-id'] = product_id
+        data["product-id"] = product_id
         push_data(data)
         print("Finished with cloud")
 
 
 def handle_errors(error):
-    logfile = open('cloud_database_communicator_error.log', 'w+')
+    logfile = open("cloud_database_communicator_error.log", "w+")
     logfile.write(str(error))
 
 
 def run():
     try:
-        read_json_and_push('last_measurement.json', 'raspberry-1')
+        read_json_and_push("last_measurement.json", "raspberry-1")
     except Exception as error:
         handle_errors(error)
