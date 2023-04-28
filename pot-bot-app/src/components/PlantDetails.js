7a7ae7aaeb10ad5c006f56c4d21fab409316b90a
AndreasFranke5 <90702500+AndreasFranke5@users.noreply.github.com>
2023-04-28 18:15:30 +0200

    Search field at least returns the name of the plant when a search has a proper common name (such as "European Silver Fir")

--- pot-bot-app/src/components/PlantDetails.js
+++ pot-bot-app/src/components/PlantDetails.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import plantSource from '../services/plantSource';
 
--- pot-bot-app/src/services/plantSource.js
+++ pot-bot-app/src/services/plantSource.js
@@ -1,75 +1,38 @@
-const apiKey = 'sk-vhKW64412e6ecbc88586';
+const apiKey = "sk-vhKW64412e6ecbc88586";
 const searchUrl = `https://perenual.com/api/species-list?page=1&key=${apiKey}`;
 const detailsUrlTemplate = `https://perenual.com/api/species/details/{{ID}}?key=${apiKey}`;
 
 async function searchPlantByCommonName(commonName) {
-    const searchResponse = await fetch(searchUrl);
-    const searchResults = await searchResponse.json();
-    const plants = searchResults.data;
-    
-    // Search for the first plant with a matching common name
-    const matchingPlant = plants.find(plant => plant.common_name.toLowerCase() === commonName.toLowerCase());
-  
-    if (matchingPlant) {
-      // Fetch the details of the matching plant
-      const detailsUrl = detailsUrlTemplate.replace('{{ID}}', matchingPlant.id);
-      const detailsResponse = await fetch(detailsUrl);
-      const details = await detailsResponse.json();
-      return [details];
-    } else {
-      return [];
-    }
-  }
-  
-  
-  
-  async function fetchPlantDetails(plantId) {
-    const detailsUrl = detailsUrlTemplate.replace('{{ID}}', plantId);
+  const searchResponse = await fetch(searchUrl);
+  const searchResults = await searchResponse.json();
+  const plants = searchResults.data;
+
+  // Search for the first plant with a matching common name
+  const matchingPlant = plants.find(
+    (plant) => plant.common_name.toLowerCase() === commonName.toLowerCase()
+  );
+
+  if (matchingPlant) {
+    // Fetch the details of the matching plant
+    const detailsUrl = detailsUrlTemplate.replace("{{ID}}", matchingPlant.id);
     const detailsResponse = await fetch(detailsUrl);
     const details = await detailsResponse.json();
     return details;
+  } else {
+    return null;
   }
-  
-  const plantSource ={
-    searchPlantByCommonName,
-    fetchPlantDetails
-  };
+}
+
+async function fetchPlantDetails(plantId) {
+  const detailsUrl = detailsUrlTemplate.replace("{{ID}}", plantId);
+  const detailsResponse = await fetch(detailsUrl);
+  const details = await detailsResponse.json();
+  return details;
+}
+
+const plantSource = {
+  searchPlantByCommonName,
+  fetchPlantDetails,
+};
+
 export default plantSource;
-// async function searchPlantByCommonName(commonName) {
-//     try {
-//       const searchResponse = await fetch(searchUrl.replace('{{QUERY}}', commonName));
-      
-//       // Add this line to log the raw response
-//       console.log('Raw search response:', await searchResponse.text());
-      
-//       const searchResults = await searchResponse.json();
-  
-//       if (searchResults.total > 0) {
-//         const plant = searchResults.data[0];
-//         return plant;
-//       } else {
-//         return null;
-//       }
-//     } catch (error) {
-//       console.error('Error in searchPlantByCommonName:', error);
-//       return null;
-//     }
-//   }
-  
-
-// async function fetchPlantDetails(plantId) {
-//   try {
-//     const detailsUrl = detailsUrlTemplate.replace('{{ID}}', plantId);
-//     const detailsResponse = await fetch(detailsUrl);
-//     const details = await detailsResponse.json();
-//     return details;
-//   } catch (error) {
-//     console.error('Error in fetchPlantDetails:', error);
-//     return null;
-//   }
-// }
-
-// export {
-//   searchPlantByCommonName,
-//   fetchPlantDetails,
-// };
