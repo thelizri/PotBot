e09bc65f89c62136803f94f9592ff475addd27d0
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-28 12:49:31 +0200

    Update

--- RasberryPi/cloud_database_communicator.py
+++ RasberryPi/cloud_database_communicator.py
@@ -1,6 +1,9 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+try:
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
+except Exception as error:
+    handle_errors(error)
 import json
 import time
 import os
@@ -8,45 +11,60 @@ import os
 abspath = os.path.dirname(os.path.abspath(__file__))
 os.chdir(abspath)
 
-# Replace 'path/to/your-service-account-key.json' with the path to the JSON file you downloaded
+# Replace 'path/to/your-service-account-key.json' with the path to the
+# JSON file you downloaded
 cred = credentials.Certificate('/home/pi/PotBot/RasberryPi/firebase-key.json')
-#cred = credentials.Certificate('C:\\Users\\karlw\\Documents\\Code\\PotBot\\firebase-key.json')
+# cred = credentials.Certificate('C:\\Users\\karlw\\Documents\\Code\\PotBot\\firebase-key.json')
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/'
 })
 
+
 def push_data(data):
-    # Replace 'your_database_path' with the path where you want to push the data
-    ref = db.reference('/users/ffJEWDC2nfMi6BFu7fS1mKkRXnC3/plants/Parasollpilea/measureData')
+    # Replace 'your_database_path' with the path where you want to push the
+    # data
+    ref = db.reference(
+        '/users/ffJEWDC2nfMi6BFu7fS1mKkRXnC3/plants/Parasollpilea/measureData')
     child = ref.child(data['date'])
     grandchild = child.child(data['time'])
     grandchild.update(data)
 
+
 def read_json_and_push(filepath, product_id):
-    
-    while True:
+
+    if True:
+        print("Cloud Start")
         if not os.path.exists(filepath):
             print("JSON file not found")
             time.sleep(5)
-            continue
+            return
 
         if os.path.getsize(filepath) == 0:
             print("Empty JSON file")
             time.sleep(5)
-            continue
-        
+            return
+
         file = open(filepath)
         data = json.load(file)
         file.close()
 
-        #Deletes content
-        #with open(filepath, 'w'):
+        # Deletes content
+        # with open(filepath, 'w'):
         #    pass
 
         data['product-id'] = product_id
         push_data(data)
-        time.sleep(5)
+        print("Finished with cloud")
 
-if __name__ == '__main__':
-    read_json_and_push('last_measurement.json', 'raspberry-1')
+
+def handle_errors(error):
+    logfile = open('cloud_database_communicator_error.log', 'w+')
+    logfile.write(str(error))
+
+
+def run():
+    try:
+        read_json_and_push('last_measurement.json', 'raspberry-1')
+    except Exception as error:
+        handle_errors(error)
