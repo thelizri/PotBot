72cd393a9e332b4219c67ef11ddd336f88a0b70e
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2023-05-03 14:29:06 +0200

    move plant.js into plantpresenter, etc

--- pot-bot-app/src/App.js
+++ pot-bot-app/src/App.js
@@ -7,35 +7,33 @@ import AddPlantPresenter from "./presenters/AddPlantPresenter";
 import {UserAuthContextProvider} from "./firebaseModel";
 import HomePresenter from "./presenters/HomePresenter";
 import ChangeUserName from "./views/ChangeUserName";
-import PlantSearch from "./components/PlantSearch";
-import { useState } from "react";
+import {useState} from "react";
 
 function App() {
-    const [personalPlantList, setPersonalPlantList] = useState([]);
-    const addPlantToPersonalList = (plant) => {
-        setPersonalPlantList([...personalPlantList, plant]);
-      };
-      return (
-        <div className='App'>
-          <Header />
-          <Router>
-            <UserAuthContextProvider>
-              <Routes>
-                <Route path='/' element={<LoginPresenter />} />
-                <Route path='/signup' element={<SignUpPresenter />} />
-                <Route path='/reset' element={<ResetPresenter />} />
-                <Route path='/name' element={<ChangeUserName />} />
-                <Route path='/home' element={<HomePresenter personalPlantList={personalPlantList} />} />
-                <Route
-                  path='/addNewPlant'
-                  element={<AddPlantPresenter addPlantToPersonalList={addPlantToPersonalList} />}
-                />
-                {/* <Route path='/home/plants' element={<PlantPresenter />} /> */}
-              </Routes>
-            </UserAuthContextProvider>
-          </Router>
-        </div>
-      );
-    }
-    
-    export default App;
+  const [personalPlantList, setPersonalPlantList] = useState([]);
+  const addPlantToPersonalList = (plant) => {
+    setPersonalPlantList([...personalPlantList, plant]);
+  };
+  return (
+    <div className='App'>
+      <Header/>
+      <Router>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route path='/' element={<LoginPresenter/>}/>
+            <Route path='/signup' element={<SignUpPresenter/>}/>
+            <Route path='/reset' element={<ResetPasswordPresenter/>}/>
+            <Route path='/name' element={<ChangeUserName/>}/>
+            <Route path='/home' element={<HomePresenter personalPlantList={personalPlantList}/>}/>
+            <Route
+              path='/addNewPlant'
+              element={<AddPlantPresenter addPlantToPersonalList={addPlantToPersonalList}/>}
+            />
+          </Routes>
+        </UserAuthContextProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
