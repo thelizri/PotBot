0303e587c1554977edafd5484bdb92cd6a2703fa
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-04 11:57:21 +0200

    update

--- RasberryPi/user_pi_syncing.py
+++ RasberryPi/user_pi_syncing.py
@@ -1,4 +1,5 @@
 from error_handler import handle_errors
+from time import sleep
 try:
     import firebase_admin
     from firebase_admin import credentials
@@ -8,24 +9,36 @@ except Exception as error:
 import os
 
 def link_pi_with_user():
-    abspath = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(abspath)
+    try:
+        abspath = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(abspath)
 
-    cred = credentials.Certificate("/home/pi/PotBot/RasberryPi/firebase-key.json")
-    firebase_admin.initialize_app(
-        cred,
-        {
-            "databaseURL": "https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/"
-        },
-    )
+        cred = credentials.Certificate("/home/pi/PotBot/RasberryPi/firebase-key.json")
+        firebase_admin.initialize_app(
+            cred,
+            {
+                "databaseURL": "https://potbot-9f9ff-default-rtdb.europe-west1.firebasedatabase.app/"
+            },
+        )
 
-    ref = db.reference("/potbots")
-    with open("product_id.txt", "r") as id_file:
-        product_id = id_file.readline()
+        ref = db.reference("/potbots")
+
+        product_id_file = open("product_id.txt", "r")
+        product_id = product_id_file.readline().strip()
         ref.update({
             product_id: ""
         })
-        #TODO
-        #Check if user id has been supplied and store it in a file.
-        #Change static references to user with user id found in above file.
-        print(ref.child(product_id).get())
+
+        while ref.child(product_id).get() == "":
+            sleep(10)
+
+        uid = ref.child(product_id).get()
+        user_id_file = open("user_id.txt", "w")
+        user_id_file.write(uid)
+
+        ref.child(product_id).delete()
+    except Exception as error:
+        handle_errors("user_pi_syncing_error.log", error)
+
+if __name__ == "__main__":
+    link_pi_with_user()
