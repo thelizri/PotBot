2e381b2f543212340d4e4feaa5353ba4bfd36570
Mahad Ahmed <95343511+mahadaahmed@users.noreply.github.com>
2023-05-04 15:23:43 +0200

    Infinite scroll (#56)

--- pot-bot-app/src/services/plantSource.js
+++ pot-bot-app/src/services/plantSource.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import { useState } from 'react';
+
 
 const API_BASE = 'https://perenual.com/api';
 const API_KEY = 'sk-vhKW64412e6ecbc88586';
 
-const searchPlants = async (searchTerm) => {
+const searchPlants = async (searchTerm, page = 1) => {
   console.log('searchPlants called with:', searchTerm);
   try {
-    const response = await axios.get(`${API_BASE}/species-list?key=${API_KEY}`);
+    const response = await axios.get(`${API_BASE}/species-list?key=${API_KEY}&page=${page}`);
     const plants = response.data.data;
 
     const filteredPlants = plants.filter(plant => {
@@ -27,6 +29,7 @@ const searchPlants = async (searchTerm) => {
 const fetchPlantDetails = async (plantId) => {
   try {
     const response = await axios.get(`${API_BASE}/species/details/${plantId}?key=${API_KEY}`);
+
     return response.data;
   } catch (error) {
     console.error('Error fetching plant details:', error);
@@ -34,44 +37,4 @@ const fetchPlantDetails = async (plantId) => {
   }
 };
 
-export default {searchPlants, fetchPlantDetails};
-
-
-// const apiKey = "sk-vhKW64412e6ecbc88586";
-// const searchUrl = `https://perenual.com/api/species-list?page=1&key=${apiKey}`;
-// const detailsUrlTemplate = `https://perenual.com/api/species/details/{{ID}}?key=${apiKey}`;
-
-// async function searchPlantByCommonName(commonName) {
-//   const searchResponse = await fetch(searchUrl);
-//   const searchResults = await searchResponse.json();
-//   const plants = searchResults.data;
-
-//   // Search for the first plant with a matching common name
-//   const matchingPlant = plants.find(
-//     (plant) => plant.common_name.toLowerCase() === commonName.toLowerCase()
-//   );
-
-//   if (matchingPlant) {
-//     // Fetch the details of the matching plant
-//     const detailsUrl = detailsUrlTemplate.replace("{{ID}}", matchingPlant.id);
-//     const detailsResponse = await fetch(detailsUrl);
-//     const details = await detailsResponse.json();
-//     return details;
-//   } else {
-//     return null;
-//   }
-// }
-
-// async function fetchPlantDetails(plantId) {
-//   const detailsUrl = detailsUrlTemplate.replace("{{ID}}", plantId);
-//   const detailsResponse = await fetch(detailsUrl);
-//   const details = await detailsResponse.json();
-//   return details;
-// }
-
-// const plantSource = {
-//   searchPlantByCommonName,
-//   fetchPlantDetails,
-// };
-
-// export default plantSource;
+export default { searchPlants, fetchPlantDetails };
--- pot-bot-app/src/views/AddPlantView.js
+++ pot-bot-app/src/views/AddPlantView.js
@@ -1,15 +1,25 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import plantSource from '../services/plantSource';
+import { ThreeDots } from 'react-loader-spinner'
 import '../styling/AddPlant.css'
 
 /*TODO:Flytta konstanter till presenter från app */
 const {searchPlants, fetchPlantDetails} = plantSource;
 
-export default function AddPlantView({addPlantToPersonalList}) {
+
+export default function AddPlantView({ addPlantToPersonalList }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [expandedPlantId, setExpandedPlantId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [, setIsSearchMade] = useState(false);
+  const [isFetchingMore, setIsFetchingMore] = useState(false);
+  const [isLoading,setIsLoading] = useState(false);
+
+
+  const observer = useRef();
+  const loaderRef = useRef();
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -37,10 +47,48 @@ export default function AddPlantView({addPlantToPersonalList}) {
       );
       console.log("Plant Details:", plantDetails);
       setSearchResults(plantDetails);
+      setIsSearchMade(true); // Set isSearchMade to true when search is made
+      setIsLoading(true)
+
     } else {
       setSearchResults([]);
     }
   };
+
+  const handleLoadMore = async () => {
+    const nextPage = currentPage + 1;
+    const results = await searchPlants(searchTerm, nextPage);
+    setCurrentPage(nextPage);
+    const plantDetails = await Promise.all(
+      results.map((plant) => fetchPlantDetails(plant.id))
+    );
+    setSearchResults([...searchResults, ...plantDetails]);
+    setIsFetchingMore(false);
+  };
+
+  useEffect(() => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver((entries) => {
+      const first = entries[0];
+      if (first.isIntersecting && !isFetchingMore) {
+        setIsFetchingMore(true);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isFetchingMore) {
+      handleLoadMore();
+    }
+  }, [isFetchingMore]);
+
+  useEffect(() => {
+    if (observer.current && searchResults.length > 0) {
+      observer.current.observe(document.querySelector(".plant-card:last-child"));
+    }
+  }, [searchResults]);
 
   return (
     <div className="addPlant">
@@ -60,16 +108,15 @@ export default function AddPlantView({addPlantToPersonalList}) {
           <button className="back-btn">Back to your plants</button>
         </Link>
       </form>
-
       <div className="search-results-grid">
-        {searchResults.map((plant) => (
+        {searchResults.map((plant, index) => (
           <div
             className="plant-card"
             key={plant.id}
             onClick={() => handlePlantClick(plant.id)}
           >
             <div key={plant.id}>
-              <img src={plant.default_image.regular_url} alt={plant.common_name} width="100" height="100"/>
+              <img src={plant.default_image.regular_url} alt={plant.common_name} width="100" height="100" />
               <p>{plant.common_name}</p>
             </div>
             {expandedPlantId === plant.id && (
@@ -82,10 +129,14 @@ export default function AddPlantView({addPlantToPersonalList}) {
                 </button>
               </div>
             )}
-            {plant.details && (
-              <div>
-                {plant.details.watering && <p>Watering: {plant.details.watering}</p>}
-                {plant.details.sunlight && <p>Sunlight: {plant.details.sunlight.join(', ')}</p>}
+            {index === searchResults.length - 1 && !isLoading && (
+              <div className="load-more" ref={loaderRef}>
+                Load More
+              </div>
+            )}
+            {isLoading && (
+              <div className="loading">
+                <ThreeDots type="ThreeDots" color="#2BAD60" height={40} width={40} />
               </div>
             )}
           </div>
@@ -93,4 +144,5 @@ export default function AddPlantView({addPlantToPersonalList}) {
       </div>
     </div>
   );
+
 }
