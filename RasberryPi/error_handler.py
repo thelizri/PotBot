fbfc81cf385b97ff592de98abd52240b44695853
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-04 16:50:47 +0200

    format change

--- RasberryPi/error_handler.py
+++ RasberryPi/error_handler.py
@@ -2,5 +2,6 @@ from datetime import datetime
 
 def handle_errors(log_file, error):
     logfile = open(log_file, "w+")
-    logfile.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    logfile.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S: "))
     logfile.write(str(error))
+    logfile.write("\n")
