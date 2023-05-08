871219d3446b2b3d7f346e95b45ae43242268998
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-08 14:36:22 +0200

    Update

--- RasberryPi/error_handler.py
+++ RasberryPi/error_handler.py
@@ -3,10 +3,7 @@ from traceback import format_exc
 import utils
 
 def handle_errors(log_file, error):
-    mode = "w"
-    if utils.check_if_file_exist_and_is_not_empty(log_file):
-        mode = "a"
-    logfile = open(log_file, mode)
+    logfile = open(log_file, "a")
     logfile.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S: "))
     logfile.write(str(error))
     logfile.write(f"\n{format_exc()}")
