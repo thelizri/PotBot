4fbbc2a28cbd4ca20cdfcc1a9fcdc70e805f0439
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-09 13:44:34 +0200

    Update

--- RasberryPi/utils.py
+++ RasberryPi/utils.py
@@ -42,6 +42,7 @@ def time_difference_in_hours(timestamp1, timestamp2):
     return time_difference.total_seconds() / 3600
 
 
+# Returns the amount of hours since the plant was last watered
 def get_hours_since_plant_was_watered():
     if check_if_file_exist_and_is_not_empty(filepath_timestamp):
         savedTime = read_timestamp_from_file(filepath_timestamp)
