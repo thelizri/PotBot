d4c69a8445a15cf7747a67f4080b9f4b9fcc2bec
Otto Heile <ottoheilee@gmail.com>
2023-05-09 14:06:09 +0200

    moved the pump controller to a separate file

--- RasberryPi/main_controller.py
+++ RasberryPi/main_controller.py
@@ -6,6 +6,7 @@ import time
 import os
 import database_manager
 import arduino_manager
+import pump_controller
 
 # import email_manager
 import user_pi_syncing
@@ -16,60 +17,6 @@ try:
     os.chdir(abspath)
 except Exception as error:
     handle_errors("main_controller_error.log", error)
-
-
-def pump_controller():
-    print("pump_controller")
-    if not utils.check_if_file_exist_and_is_not_empty("settings.json"):
-        return None
-
-    file = open("settings.json")
-    data = json.load(file)
-    file.close()
-
-    def manual():
-        if data["water"] == 1:
-            measurements = json.load(open("last_measurement.json"))
-            measurements = list(measurements.values())[0]
-            if measurements["soilMoisture"] < data["soil_moisture"]:
-                arduino_manager.turn_on_water_pump(data["amount"])
-                print("Turned on water pump")
-            data["water"] = 0
-            file = open("settings.json", "w")
-            json.dump(data, file)
-            file.close()
-
-    def automatic():
-        measurements = json.load(open("last_measurement.json"))
-        measurements = list(measurements.values())[0]
-        if measurements["soilMoisture"] < data["soil_moisture"]:
-            arduino_manager.turn_on_water_pump(data["amount"])
-            print("Turned on water pump")
-
-    def frequency():
-        if utils.check_if_file_exist_and_is_not_empty(
-            "latest_time_plant_was_watered.json"
-        ):
-            savedTime = utils.read_timestamp_from_file(
-                "latest_time_plant_was_watered.json"
-            )
-            currentTime = utils.get_timestamp()
-            difference = utils.time_difference_in_hours(savedTime, currentTime)
-            if difference >= data["frequency"]:
-                arduino_manager.turn_on_water_pump(data["amount"])
-                print("Turned on water pump")
-        else:
-            arduino_manager.turn_on_water_pump(data["amount"])
-            print("Turned on water pump")
-
-    if data["type"] == "Manual":
-        manual()
-    elif data["type"] == "Automatic":
-        automatic()
-    elif data["type"] == "Frequent":
-        frequency()
-    else:
-        print("Incorrect type. Check the settings.")
 
 
 def check_water_level():
@@ -117,12 +64,12 @@ def run():
         pusher = Thread(target=database_manager.run)
         pusher.start()
 
+        # Turns the pump on/off when necessary
+        print("Create pump controller runner")
+        pump = Thread(target=pump_controller.run)
+        pump.start()
+
         print("Created all threads")
-        while True:
-            print("inside while loop")
-            pump_controller()
-            print("Pump controller finished")
-            time.sleep(20)
     except Exception as error:
         handle_errors("main_controller_error.log", error)
 
