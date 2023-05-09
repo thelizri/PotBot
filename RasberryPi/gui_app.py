b35b7f6fa93e6f3c161967b644687d9add0a4123
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-09 16:28:34 +0200

    Update

--- RasberryPi/gui_app.py
+++ RasberryPi/gui_app.py
@@ -2,38 +2,44 @@ from tkinter import *
 from tkinter import ttk
 import json
 
-counter = -1
+counter = 0
+
 
 def update_labels(label_vars, window):
-
-    data = None
-    with open("last_measurement.json", "r") as file:
-        data = json.load(file)
     global counter
     counter += 1
-    label_vars[0].set("Water Level: "+str(data["waterLevel"])+" "+str(counter))
-    label_vars[1].set("UV Light: "+str(data["uvLight"])+" "+str(counter))
-    label_vars[2].set("Soil Moisture: "+str(data["soilMoisture"])+" "+str(counter))
-    label_vars[3].set("Temperature: "+str(data["temperature"])+" "+str(counter))
+    data = None
+
+    with open("last_measurement.json", "r") as file:
+        data = json.load(file)
+        data = data.values()[0]
+
+    label_vars[0].set("Water Level: " + str(data["waterLevel"]) + " " + str(counter))
+    label_vars[1].set("UV Light: " + str(data["uvIntensity"]) + " " + str(counter))
+    label_vars[2].set(
+        "Soil Moisture: " + str(data["soilMoisture"]) + " " + str(counter)
+    )
+    label_vars[3].set("Temperature: " + str(data["temperature"]) + " " + str(counter))
     # Schedule the next update
-    window.after(1000, lambda : update_labels(label_vars, window))
+    window.after(1000, lambda: update_labels(label_vars, window))
+
 
 def run():
     bg_color = "#94C973"
     fg_color = "#000209"
 
     window = Tk()
-    window.attributes('-fullscreen', True)
+    window.attributes("-fullscreen", True)
     window.title("PotBot")
     window.configure(bg=bg_color)
 
     style = ttk.Style()
-    style.configure('TFrame', background=bg_color)
+    style.configure("TFrame", background=bg_color)
 
     frame = ttk.Frame(window, padding=10, style="TFrame")
     frame.grid()
 
-    myfont = ('Times New Roman', 22, 'bold')
+    myfont = ("Times New Roman", 22, "bold")
 
     labels_text = ["Water Level: ", "UV light: ", "Soil moisture: ", "Temperature: "]
     label_vars = []
@@ -41,11 +47,15 @@ def run():
     for i, text in enumerate(labels_text):
         label_var = StringVar()
         label_var.set(text)
-        label = Label(frame, textvariable=label_var, font=myfont, bg=bg_color, fg=fg_color)
-        label.grid(column=0, row=i, pady=5, padx=(0, 10), sticky='w')
+        label = Label(
+            frame, textvariable=label_var, font=myfont, bg=bg_color, fg=fg_color
+        )
+        label.grid(column=0, row=i, pady=5, padx=(0, 10), sticky="w")
         label_vars.append(label_var)
 
-    ttk.Button(frame, text="Quit", command=window.destroy).grid(column=0, row=4, pady=10)
+    ttk.Button(frame, text="Quit", command=window.destroy).grid(
+        column=0, row=4, pady=10
+    )
 
     # Centering the frame on the window
     frame.update_idletasks()
@@ -61,5 +71,6 @@ def run():
     update_labels(label_vars, window)
     window.mainloop()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run()
