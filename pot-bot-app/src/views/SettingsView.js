51317d7e4f60a1442634f30eacbe7a68bdb5d738
Mahad Ahmed <95343511+mahadaahmed@users.noreply.github.com>
2023-05-10 10:05:23 +0200

    Notification settings, added in firebase (#77)

---
+++ pot-bot-app/src/views/SettingsView.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Settings from "../styling/Settings.css";
+import { notificationToggle, useAuth } from '../firebaseModel';
+
+function SettingsView() {
+    const {user} = useAuth();
+
+    const handleNotificationToggle = async (event) => {
+        const toggleValue = event.target.checked;
+        await notificationToggle(user, toggleValue);
+      };
+      
+      
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      <label htmlFor="notificationToggle">Receive notifications</label>
+      <input
+        type="checkbox"
+        id="notificationToggle"
+        onChange={handleNotificationToggle}
+      />
+    </div>
+  );
+}
+export default SettingsView;
