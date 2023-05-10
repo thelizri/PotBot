3137c6d4a72724e925eea726b84e05114a1d968b
Mahad Ahmed <95343511+mahadaahmed@users.noreply.github.com>
2023-05-10 10:05:23 +0200

    Notification settings, added in firebase (#77)

--- pot-bot-app/src/App.js
+++ pot-bot-app/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import LoginPresenter from "./presenters/LoginPresenter";
 import SignUpPresenter from "./presenters/SignUpPresenter";
@@ -7,28 +7,32 @@ import AddPlantPresenter from "./presenters/AddPlantPresenter";
 import HomePresenter from "./presenters/HomePresenter";
 import HistoryPresenter from "./presenters/HistoryPresenter";
 import ChangeUserName from "./views/ChangeUserName";
-import {UserAuthContextProvider} from "./firebaseModel";
-import './styling/App.css'
+import { UserAuthContextProvider } from "./firebaseModel";
+import SideMenuView from "./views/SideMenuView";
+import SettingsView from "./views/SettingsView";
+import "./styling/App.css";
 
-//Flyttade konstanter till AddPlantPresenter.js
 function App() {
-
   return (
-    <div className='App'>
-      <Header/>
+    <div className="App">
       <Router>
         <UserAuthContextProvider>
+          {/* Render the side menu view on all pages except login and signup */}
+          {window.location.pathname !== "/signup" &&
+          window.location.pathname !== "/login" && <SideMenuView />}
+          <Header />
           <Routes>
-            <Route path='/' element={<LoginPresenter/>}/>
-            <Route path='/signup' element={<SignUpPresenter/>}/>
-            <Route path='/reset' element={<ResetPasswordPresenter/>}/>
-            <Route path='/name' element={<ChangeUserName/>}/>
-            <Route path='/home' element={<HomePresenter/>}/>
+            <Route path="/" element={<LoginPresenter />} />
+            <Route path="/signup" element={<SignUpPresenter />} />
+            <Route path="/reset" element={<ResetPasswordPresenter />} />
+            <Route path="/name" element={<ChangeUserName />} />
+            <Route path="/home" element={<HomePresenter />} />
+            <Route path="/settings" element={<SettingsView />} />
             <Route
-              path='/addNewPlant'
-              element={<AddPlantPresenter/>}
+              path="/addNewPlant"
+              element={<AddPlantPresenter />}
             />
-            <Route path='/history' element={<HistoryPresenter/>}/>
+            <Route path="/history" element={<HistoryPresenter />} />
           </Routes>
         </UserAuthContextProvider>
       </Router>
