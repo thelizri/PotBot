933acb1e30f0c3c4c17fd64e06939316b7455b28
Otto Heile <ottoheilee@gmail.com>
2023-05-10 18:27:51 +0200

    added db listener

--- RasberryPi/user_pi_syncing.py
+++ RasberryPi/user_pi_syncing.py
@@ -3,15 +3,14 @@ from time import sleep
 import os
 
 try:
-    import firebase_admin
-    from firebase_admin import credentials
-    from firebase_admin import db
-
     abspath = os.path.dirname(os.path.abspath(__file__))
     os.chdir(abspath)
 except Exception as error:
     handle_errors("user_pi_syncing_error.log", error)
 
+db = None
+ref = None
+product_id = None
 
 def is_linked_with_user(database):
     try:
@@ -29,37 +28,39 @@ def is_linked_with_user(database):
         return False
 
 
-def link_pi_with_user(database):
-    try:
-        ref = database.reference("/potbots")
-        product_id_file = open("product.id", "r")
-        product_id = product_id_file.readline().strip()
-        ref.update({product_id: ""})
+def _link_pi_with_user(event):
+    data = event.data
+    if data == None:
+        return
 
-        while ref.child(product_id).get() == "":
-            print("Sleep 10")
-            sleep(10)
+    uid = data["uid"]
+    user_id_file = open("user.id", "w")
+    user_id_file.write(uid)
 
-        uid = ref.child(product_id).child("uid").get()
-        user_id_file = open("user.id", "w")
-        user_id_file.write(uid)
+    plant = data["plant"]
+    plant_file = open("plant.id", "w")
+    plant_file.write(plant)
 
-        plant = ref.child(product_id).child("plant").get()
-        plant_file = open("plant.id", "w")
-        plant_file.write(plant)
-
-        ref.child(product_id).delete()
-        database.reference(f"/users/{uid}/plants/{plant}").update(
-            {"productID": product_id}
-        )
-    except Exception as error:
-        handle_errors("user_pi_syncing_error.log", error)
+    db.reference(f"/users/{uid}/plants/{plant}").update(
+        {"productID": product_id}
+    )
+    ref.child(product_id).delete()
 
 
 def run(database):
-    if not is_linked_with_user(database):
-        print("Is not linked with user")
-        link_pi_with_user(database)
+    try:
+        db = database
+        if not is_linked_with_user(database):
+            print("Is not linked with user")
+
+            ref = database.reference("/potbots")
+            product_id_file = open("product.id", "r")
+            product_id = product_id_file.readline().strip()
+            ref.update({product_id: ""})
+
+            ref.listen(_link_pi_with_user)
+    except Exception as error:
+        handle_errors("user_pi_syncing_error.log", error)
 
 
 if __name__ == "__main__":
