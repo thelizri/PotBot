125509fccb39e4b8af102b633213f448c92eab63
AndreasFranke5 <90702500+AndreasFranke5@users.noreply.github.com>
2023-05-11 18:23:00 +0200

    Capitalized first letter of each word in search

--- pot-bot-app/src/services/plantSource.js
+++ pot-bot-app/src/services/plantSource.js
@@ -1,29 +1,38 @@
-import {get, ref} from "firebase/database";
-import {db} from "../firebaseModel";
+import { get, ref } from "firebase/database";
+import { db } from "../firebaseModel";
 
-const API_BASE = 'https://perenual.com/api';
-const API_KEY = 'sk-vhKW64412e6ecbc88586';
+const API_BASE = "https://perenual.com/api";
+const API_KEY = "sk-vhKW64412e6ecbc88586";
 
-
-  export const searchPlants = async (searchTerm) => {
+export const searchPlants = async (searchTerm) => {
   const dbRef = ref(db, "plantsData/species_data_detailed");
   const snapshot = await get(dbRef);
   const plantData = snapshot.val();
 
   const results = Object.values(plantData).filter(
     (plant) =>
-    plant.common_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    plant.scientific_name.some(name => name.toLowerCase().includes(searchTerm.toLowerCase())) ||
-    (plant.other_name && plant.other_name.some(name => name.toLowerCase().includes(searchTerm.toLowerCase())))
+      plant.common_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      plant.scientific_name.some((name) => name.toLowerCase().includes(searchTerm.toLowerCase())) ||
+      (plant.other_name && plant.other_name.some((name) => name.toLowerCase().includes(searchTerm.toLowerCase())))
   );
 
-  return results;
-}
+  const capitalizedResults = results.map((plant) => {
+    return {
+      ...plant,
+      common_name: plant.common_name
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" "),
+    };
+  });
+
+  return capitalizedResults;
+};
 
 export const fetchPlantDetails = async (plantId) => {
   const dbRef = ref(db, `plantsData/species_data_detailed/${plantId - 1}`);
   const snapshot = await get(dbRef);
   return snapshot.val();
-}
+};
 
 export default { searchPlants, fetchPlantDetails };
