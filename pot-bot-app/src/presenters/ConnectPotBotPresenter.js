6ed817fef3e339fde884d8ab4112a8fc3195b59b
Mahad Ahmed <mahadahmed_@outlook.com>
2023-05-11 17:10:04 +0200

    Connect now works and redirects

--- pot-bot-app/src/presenters/ConnectPotBotPresenter.js
+++ pot-bot-app/src/presenters/ConnectPotBotPresenter.js
@@ -1,45 +1,76 @@
-import React, {useEffect, useState} from "react";
-import {connectPotBot, db, useAuth} from "../firebaseModel";
+import React, { useEffect, useState } from "react";
+import { connectPotBot, db, useAuth } from "../firebaseModel";
 import ConnectPotBotView from "../views/ConnectPotBotView";
-import {useLocation} from "react-router-dom";
-import {onChildChanged, ref} from "firebase/database";
+import { useLocation, useNavigate } from "react-router-dom";
+import { onChildChanged, ref } from "firebase/database";
 
 export default function ConnectPotBotPresenter() {
-  const {user} = useAuth()
+  const { user } = useAuth();
   const [connected, setConnected] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [productID, setProductID] = useState('');
-  let {state} = useLocation()
+  const [productID, setProductID] = useState("");
+  const { state } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (loading === true && productID) {
-      connectPotBot(productID, {uid: user.uid, plant: state.plantName}).then(() => {
-        connectionListener(user, state.plantName)
-      })
+    async function connectPotBotAndCheck() {
+      try {
+        console.log('Connecting PotBot...');
+        await connectPotBot(productID, { uid: user.uid, plant: state.plantName });
+        console.log('PotBot connected!');
+        const dbRef = ref(
+          db,
+          `users/${user.uid}/plants/${state.plantName}`
+        );
+        onChildChanged(dbRef, (snapshot) => {
+          if (snapshot.exists()) {
+            console.log('Plant connected!');
+            setConnected(true);
+            setTimeout(() => {
+              setLoading(false);
+              setTimeout(() => {
+                navigate("/home");
+              }, 1000); 
+            }, 3000);
+          }
+        });
+        console.log("Event listener added.");
+      } catch (error) {
+        console.error("Failed to connect PotBot", error);
+      }
     }
-  }, [user, productID, state, loading])
+    console.log('Loading:', loading, 'Product ID:', productID);
+    if (loading && productID) {
+      connectPotBotAndCheck();
+    }
+  }, [user, productID, state, loading, navigate]);
+  
 
   function connectButtonHandler() {
-    setLoading(true)
-  }
-
-  function connectionListener(user, plantName) {
-    const dbRef = ref(db, `user/${user.uid}/plants/${plantName}`)
-    return onChildChanged(dbRef, (snapshot) => {
-      console.log(snapshot.val())
-      setConnected(true)
-    }, {
-      onlyOnce: true
-    })
+    setLoading(true);
   }
 
   return (
     <div>
-      {!connected && !loading && <ConnectPotBotView loading={loading} productID={productID} setProductID={setProductID}
-                                                    handleSubmit={connectButtonHandler}/>}
-      {loading && connected && <ConnectPotBotView loading={loading}/>}
-
+      {!connected && !loading && (
+        <ConnectPotBotView
+          loading={loading}
+          productID={productID}
+          setProductID={setProductID}
+          handleSubmit={connectButtonHandler}
+        />
+      )}
+      {loading && !connected && (
+        <div style={{ textAlign: "center" }}>
+          <h2>Connecting PotBot...</h2>
+          <img src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif" alt="Loading" />
+        </div>
+      )}
+      {loading && connected && (
+        <div style={{ textAlign: "center" }}>
+          <h2>Your PotBot is now connected!</h2>
+        </div>
+      )}
     </div>
-  )
-
+  );
 }
