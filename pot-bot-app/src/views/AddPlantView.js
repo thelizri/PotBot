4011fe04a65fda2caa0d5379fc1103b0f05cccc7
AndreasFranke5 <90702500+AndreasFranke5@users.noreply.github.com>
2023-05-11 18:22:55 +0200

    Capitalized first letter of each word in search

--- pot-bot-app/src/views/AddPlantView.js
+++ pot-bot-app/src/views/AddPlantView.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Link} from "react-router-dom";
-import plantSource from '../services/plantSource';
-import {ThreeDots} from 'react-loader-spinner'
-import '../styling/AddPlant.css'
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import plantSource from "../services/plantSource";
+import { ThreeDots } from "react-loader-spinner";
+import "../styling/AddPlant.css";
 import { get, ref } from "firebase/database";
 import { db } from "../firebaseModel";
 import { searchPlants, fetchPlantDetails } from "../services/plantSource";
@@ -86,40 +86,38 @@ export default function AddPlantView({ addPlantToPersonalList }) {
     const results = await searchPlants(searchTerm, nextPage);
     setCurrentPage(nextPage);
     const validIDs = results.filter((plant) => isIDValid(plant.id));
-    const plantDetails = await Promise.all(
-      validIDs.map((plant) => fetchPlantDetails(plant.id))
-    );
+    const plantDetails = await Promise.all(validIDs.map((plant) => fetchPlantDetails(plant.id)));
     setSearchResults((prevResults) => [...prevResults, ...plantDetails]);
     setIsFetchingMore(false);
   };
-  
+
   useEffect(() => {
     const observerOptions = {
       root: null,
       rootMargin: "0px",
       threshold: 1.0,
     };
-  
+
     const observerCallback = (entries) => {
       const first = entries[0];
       if (first.isIntersecting && !isFetchingMore) {
         setIsFetchingMore(true);
       }
     };
-  
+
     const observer = new IntersectionObserver(observerCallback, observerOptions);
-  
+
     if (loaderRef.current) {
       observer.observe(loaderRef.current);
     }
-  
+
     return () => {
       if (loaderRef.current) {
         observer.unobserve(loaderRef.current);
       }
     };
   }, [isFetchingMore]);
-  
+
   useEffect(() => {
     if (observer.current && searchResults.length > 0) {
       observer.current.observe(loaderRef.current);
@@ -133,57 +131,40 @@ export default function AddPlantView({ addPlantToPersonalList }) {
         <p>First choose what kind of plant you have and we will calibrate the optimal conditions for it</p>
       </div>
       <form className="plant-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Choose your plant"
-          value={searchTerm}
-          onChange={handleChange}
-        />
+        <input type="text" placeholder="Choose your plant" value={searchTerm} onChange={handleChange} />
         <button type="submit">Search</button>
         <Link to="/home">
           <button className="back-btn">Back to your plants</button>
         </Link>
       </form>
       <div className="search-results-grid">
-      {searchResults.map((plant, index) => (
-  <div
-    className="plant-card"
-    key={plant.id}
-    onClick={() => handlePlantClick(plant.id)}
-  >
-    <div key={plant.id}>
-      {plant.default_image && (
-        <img
-          src={plant.default_image.regular_url}
-          alt={plant.common_name}
-          width="100"
-          height="100"
-        />
-      )}
-      <p>{plant.common_name}</p>
-    </div>
-    {expandedPlantId === plant.id && (
-      <div className="plant-dropdown">
-        <button
-          className="add-plant-button"
-          onClick={() => handleAddPlantButtonClick(plant)}
-        >
-          Add to my plants
-        </button>
-      </div>
-    )}
-    {index === searchResults.length - 1 && !isLoading && (
-      <div className="load-more" ref={loaderRef}>
-        Load More
-      </div>
-    )}
-    {isLoading && (
-      <div className="loading">
-        <ThreeDots type="ThreeDots" color="#2BAD60" height={40} width={40} />
-      </div>
-    )}
-  </div>
-))}
+        {searchResults.map((plant, index) => (
+          <div className="plant-card" key={plant.id} onClick={() => handlePlantClick(plant.id)}>
+            <div key={plant.id}>
+              {plant.default_image && (
+                <img src={plant.default_image.regular_url} alt={plant.common_name} width="100" height="100" />
+              )}
+              <p>{plant.common_name}</p>
+            </div>
+            {expandedPlantId === plant.id && (
+              <div className="plant-dropdown">
+                <button className="add-plant-button" onClick={() => handleAddPlantButtonClick(plant)}>
+                  Add to my plants
+                </button>
+              </div>
+            )}
+            {index === searchResults.length - 1 && !isLoading && (
+              <div className="load-more" ref={loaderRef}>
+                Load More
+              </div>
+            )}
+            {isLoading && (
+              <div className="loading">
+                <ThreeDots type="ThreeDots" color="#2BAD60" height={40} width={40} />
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
